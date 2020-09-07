9d364fa8e43eb872b99430fa93f6b67a22a9986c
Alex Yoon <wonsangalex@gmail.com>
2020-09-07 12:33:31 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import "./User_info.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isFetching: true,
+    user_profiles: [],
+    key_value : [ "id", "name", "username", "email", "address"]
+  };
+
+    async fetch_Data(url){
+      const user_profiles = await axios.get(url);
+      this.setState({user_profiles, isFetching: false });
+    }
+    render() {
+      const {user_profiles, isFetching, key_value } = this.state;
+      return (
+        <div className="container">
+          <button>Post</button>
+          <button>Comments</button>
+          <button>Albums</button>
+          <button>Photos</button>
+          <button onClick={() => this.fetch_Data("https://jsonplaceholder.typicode.com/todos")}>To do</button>
+          <button onClick={() => this.fetch_Data("https://jsonplaceholder.typicode.com/users")}>Users</button>
+
+        <div className="user_data">
+          {
+            isFetching ? (
+              <div className="loader">
+                <span className="loader__text">Loading...</span>
+              </div>
+            ) : (
+              console.log(user_profiles),
+              Object.keys(user_profiles.data).map(([key, i])=>(
+                <div className="user_info">
+                      <div>{key} : {user_profiles.data[key].name}</div>
+                      <div>{key} : {user_profiles.data[key].username}</div>
+                      <div>{key} : {user_profiles.data[key].email}</div>
+
+                    </div>
+                  // Object.keys(user_profiles.data[key]).map((cate, i) => (
+                  //   <div className="user_info">
+                  //     <div>{cate} : {user_profiles.data[key].username}</div>
+                  //   </div>
+                  // ))
+              ))
+            )
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
