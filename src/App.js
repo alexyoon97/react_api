03d3aa55d1d856d46b041787c0c0302e81b5496f
Alex Yoon <wonsangalex@gmail.com>
2020-09-08 15:21:17 -0700

    style updated

--- src/App.js
+++ src/App.js
@@ -1,53 +1,85 @@
 import React from "react";
 import "./App.css";
 import axios from "axios";
-import "./User_info.css"
+import "./User_info.css";
 
 class App extends React.Component {
   state = {
     isFetching: true,
     user_profiles: [],
-    key_value : [ "id", "name", "username", "email", "address"]
+    key_value: ["id", "name", "username", "email", "address"],
   };
 
-    async fetch_Data(url){
-      const user_profiles = await axios.get(url);
-      this.setState({user_profiles, isFetching: false });
-    }
-    render() {
-      const {user_profiles, isFetching, key_value } = this.state;
-      return (
-        <div className="container">
-          <button onClick={() => this.fetch_Data("https://jsonplaceholder.typicode.com/posts")}>Post</button>
-          <button onClick={() => this.fetch_Data("https://jsonplaceholder.typicode.com/comments")}>Comments</button>
-          <button onClick={() => this.fetch_Data("https://jsonplaceholder.typicode.com/albums")}>Albums</button>
-          <button onClick={() => this.fetch_Data("https://jsonplaceholder.typicode.com/photos")}>Photos</button>
-          <button onClick={() => this.fetch_Data("https://jsonplaceholder.typicode.com/todos")}>To do</button>
-          <button onClick={() => this.fetch_Data("https://jsonplaceholder.typicode.com/users")}>Users</button>
+  async fetch_Data(url) {
+    const user_profiles = await axios.get(url);
+    this.setState({ user_profiles, isFetching: false });
+  }
+  render() {
+    const { user_profiles, isFetching, key_value } = this.state;
+    return (
+      <div className="container">
+        <div className="api_call_buttons">
+          <button
+            onClick={() =>
+              this.fetch_Data("https://jsonplaceholder.typicode.com/posts")
+            }
+          >
+            Post
+          </button>
+          <button
+            onClick={() =>
+              this.fetch_Data("https://jsonplaceholder.typicode.com/comments")
+            }
+          >
+            Comments
+          </button>
+          <button
+            onClick={() =>
+              this.fetch_Data("https://jsonplaceholder.typicode.com/albums")
+            }
+          >
+            Albums
+          </button>
+          <button
+            onClick={() =>
+              this.fetch_Data("https://jsonplaceholder.typicode.com/photos")
+            }
+          >
+            Photos
+          </button>
+          <button
+            onClick={() =>
+              this.fetch_Data("https://jsonplaceholder.typicode.com/todos")
+            }
+          >
+            To do
+          </button>
+          <button
+            onClick={() =>
+              this.fetch_Data("https://jsonplaceholder.typicode.com/users")
+            }
+          >
+            Users
+          </button>
+        </div>
 
         <div className="user_data">
-          {
-            isFetching ? (
-              <div className="loader">
-                <span className="loader__text">Loading...</span>
+          {isFetching ? (
+            <div className="loader">
+              <span className="loader__text">Please Choose one</span>
+            </div>
+          ) : (
+            (console.log(user_profiles),
+            Object.keys(user_profiles.data).map(([key, i]) => (
+              <div className="user_info">
+                {Object.keys(user_profiles.data[key]).map((cate, i) => (
+                  <div>
+                    {cate} : {user_profiles.data[key].username}
+                  </div>
+                ))}
               </div>
-            ) : (
-              console.log(user_profiles),
-              Object.keys(user_profiles.data).map(([key, i])=>(
-                <div className="user_info">
-                      <div>{key} : {user_profiles.data[key].name}</div>
-                      <div>{key} : {user_profiles.data[key].username}</div>
-                      <div>{key} : {user_profiles.data[key].email}</div>
-
-                    </div>
-                  // Object.keys(user_profiles.data[key]).map((cate, i) => (
-                  //   <div className="user_info">
-                  //     <div>{cate} : {user_profiles.data[key].username}</div>
-                  //   </div>
-                  // ))
-              ))
-            )
-          }
+            )))
+          )}
         </div>
       </div>
     );
